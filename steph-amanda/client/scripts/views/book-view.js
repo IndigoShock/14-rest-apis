dd2eaa7a2dc911c923123d591bb5c67bc76f38ab
Stephen <stephendeanharper@gmail.com>
2018-04-27 13:08:33 -0700

    completed comments

---
+++ steph-amanda/client/scripts/views/book-view.js
@@ -0,0 +1,132 @@
+'use strict';
+var app = app || {};
+
+(function(module) {
+  $('.icon-menu').on('click', function(event) {
+    $('.nav-menu').slideToggle(350);
+  });
+
+  function resetView() {
+    $('.container').hide();
+    $('.nav-menu').slideUp(350);
+  }
+
+  const bookView = {};
+
+  bookView.initIndexPage = function(ctx, next) {
+    resetView();
+    $('.book-view').show();
+    $('#book-list').empty();
+    module.Book.all.forEach(book => $('#book-list').append(book.toHtml()));
+    next();
+  };
+
+  bookView.initDetailPage = function(ctx, next) {
+    resetView();
+    $('.detail-view').show();
+    $('.book-detail').empty();
+    let template = Handlebars.compile($('#book-detail-template').text());
+    $('.book-detail').append(template(ctx.book));
+
+    $('#update-btn').on('click', function() {
+      page(`/books/${$(this).data('id')}/update`);
+    });
+
+    $('#delete-btn').on('click', function() {
+      module.Book.destroy($(this).data('id'));
+    });
+    next();
+  };
+
+  bookView.initCreateFormPage = function() {
+    resetView();
+    $('.create-view').show();
+    $('#create-form').on('submit', function(event) {
+      event.preventDefault();
+
+      let book = {
+        title: event.target.title.value,
+        author: event.target.author.value,
+        isbn: event.target.isbn.value,
+        image_url: event.target.image_url.value,
+        description: event.target.description.value,
+      };
+
+      module.Book.create(book);
+    });
+  };
+
+  bookView.initUpdateFormPage = function(ctx) {
+    resetView();
+    $('.update-view').show();
+    $('#update-form input[name="title"]').val(ctx.book.title);
+    $('#update-form input[name="author"]').val(ctx.book.author);
+    $('#update-form input[name="isbn"]').val(ctx.book.isbn);
+    $('#update-form input[name="image_url"]').val(ctx.book.image_url);
+    $('#update-form textarea[name="description"]').val(ctx.book.description);
+
+    $('#update-form').on('submit', function(event) {
+      event.preventDefault();
+
+      let book = {
+        book_id: ctx.book.book_id,
+        title: event.target.title.value,
+        author: event.target.author.value,
+        isbn: event.target.isbn.value,
+        image_url: event.target.image_url.value,
+        description: event.target.description.value,
+      };
+
+      module.Book.update(book, book.book_id);
+    });
+  };
+
+  // COMMENT: What is the purpose of this method?
+  // The purpose of this method is to use the search form. To find things. It resets the view container and shows the search form.
+  bookView.initSearchFormPage = function() {
+    resetView();
+    $('.search-view').show();
+    $('#search-form').on('submit', function(event) {
+      // COMMENT: What default behavior is being prevented here?
+      // The preventDefault stops the page from resetting upon event.
+      event.preventDefault();
+
+      // COMMENT: What is the event.target, below? What will happen if the user does not provide the information needed for the title, author, or isbn properties?
+      // The event.target is on submit. If there is no user input, it will give an empty string ('').
+      let book = {
+        title: event.target.title.value || '',
+        author: event.target.author.value || '',
+        isbn: event.target.isbn.value || '',
+      };
+
+      module.Book.find(book, bookView.initSearchResultsPage);
+
+      // COMMENT: Why are these values set to an empty string?
+      // These values are set to an empty string because it will fill it in, even if the user does not provide data input.
+      event.target.title.value = '';
+      event.target.author.value = '';
+      event.target.isbn.value = '';
+    });
+  };
+
+  // COMMENT: What is the purpose of this method?
+  // The purpose of this method is to show search results.
+  bookView.initSearchResultsPage = function() {
+    resetView();
+    $('.search-results').show();
+    $('#search-list').empty();
+
+    // COMMENT: Explain how the .forEach() method is being used below.
+    // The .forEach method is iterating through book for the specific book and is appending this to the search-list id in an html format. Once it finds this info, it will show the info upon click.
+    module.Book.all.forEach(book => $('#search-list').append(book.toHtml()));
+    $('.detail-button a').text('Add to list').attr('href', '/');
+    $('.detail-button').on('click', function(e) {
+      // COMMENT: Explain the following line of code.
+      // This line of code is appending 'this' to the title, author and isbn, which are parents of each other (thus why there are 3). And it will send this data to bookid.
+      module.Book.findOne($(this).parent().parent().parent().data('bookid'));
+    });
+  };
+
+  module.bookView = bookView;
+})(app);
+
